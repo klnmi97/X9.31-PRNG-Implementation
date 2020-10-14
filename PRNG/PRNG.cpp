7d50d5244fc14d498ca1a037e7d9cdf1f042c833
Michael Klunko <michael.klunko@gmail.com>
2020-10-14 15:53:33 +0200

    Clean-up and documentation

--- PRNG/PRNG.cpp
+++ PRNG/PRNG.cpp
@@ -1,5 +1,25 @@
-// PRNG.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+---------------------------------------------------------------------------
+Copyright (c) 2020, Mykhailo Klunko.
+
+The redistribution and use of this software (with or without changes)
+is allowed without the payment of fees or royalties provided that:
+
+  source code distributions include the above copyright notice, this
+  list of conditions and the following disclaimer;
+
+  binary distributions include the above copyright notice, this list
+  of conditions and the following disclaimer in their documentation.
+
+This software is provided 'as is' with no explicit or implied warranties
+in respect of its operation, including, but not limited to, correctness
+and fitness for purpose.
+---------------------------------------------------------------------------
+
+ This is an implementation of X9.31 Pseudorandom number generator (PRNG)
+ which is based on the AES implementation by Brian Gladman.
+ The mentioned AES project can be found here: https://github.com/BrianGladman/aes.
+*/
 
 #include <iostream>
 #include "math.h"
@@ -13,19 +33,32 @@ static unsigned char* V;
 static unsigned char* DT;
 static unsigned char* K;
 
-
+/**
+ * @brief Converts char numbers to hex numbers with the same value
+ * @param value pointer to array of char numbers to be changed
+ */
 void char_to_hex(unsigned char* value) {
     for (int i = 0; i < strlen((char*)value); i++) {
         value[i] = value[i] - '0';
     }
 }
 
+/**
+ * @brief xor to values. All the input values should be the same size
+ * @param a input value
+ * @param b input value
+ * @param out output array
+ * @param len size of input and output values
+ */
 void xor_char(unsigned char* a, unsigned char* b, unsigned char* out, int len) {
     for (int i = len - 1; i >= 0; i--) {
         out[i] = a[i] ^ b[i];
     }
 }
 
+/**
+ * @brief Increment DT vector value
+ */
 void increment_DT() {
     int current_block = BLOCK_SIZE - 1;
     while (current_block >= 0) {
@@ -40,17 +73,24 @@ void increment_DT() {
     }
 }
 
+/**
+ * @brief Initialize inner state of the X9.31 PRNG
+ * @param seed 128-bit value of seed
+ * @param key 128-bit value for key
+ */
 void init_prng(unsigned char* seed, unsigned char* key) {
     V = seed;
     K = key;
-    unsigned char init_value[16] = { 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 
-        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 };
     DT = new unsigned char[16];
     memset(DT, 0, 16);
     I = new unsigned char[16];
     R = new unsigned char[16];
 }
 
+/**
+ * @brief generate 128 bits of pseudo random data
+ * @return 128 bits of random data contained in the array
+ */
 unsigned char* x931_rand() {
 
     aes_encrypt_ctx ctx[1];
@@ -74,8 +114,6 @@ unsigned char* x931_rand() {
 
 int main()
 {
-    unsigned long l_seed = 0x2328862328862328;
-    unsigned long l_key = 0x8232688232688232;
 
     // TODO: provide custom seed+key
     char seed[17] = "2328862328862328";
@@ -85,7 +123,6 @@ int main()
     char_to_hex((unsigned char*)key);
 
     init_prng((unsigned char*)seed, (unsigned char*)key);
-
 
     FILE* file_ptr;
     file_ptr = fopen("F.bin", "wb");
